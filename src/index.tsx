b4e3ea5108ea0be10b11be909c9ebf1c7fe3cc78
yulcahnshin <yulchanshin@gmail.com>
2025-10-29 02:47:25 -0400

    added css bundling using js wrapper, and updated WASM load method

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect, useRef } from 'react';
 import ReactDOM from "react-dom/client";
 import * as esbuild from 'esbuild-wasm'
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
+import { fetchPlugin } from './plugins/fetch-plugin';
 
 const el = document.getElementById("root");
 
@@ -16,7 +17,7 @@ const App = () => {
   const startService = async () => {
     ref.current = await esbuild.startService({
       worker: true,
-      wasmURL: '/esbuild.wasm'
+      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm'
     });
 
   }
@@ -34,7 +35,10 @@ const App = () => {
       entryPoints: ['index.js'],
       bundle: true,
       write: false,
-      plugins: [unpkgPathPlugin()],
+      plugins: [
+        unpkgPathPlugin(),
+        fetchPlugin(input)
+      ],
       define: {
         'process.env.NODE_ENV': '"production"',
         global: 'window'
