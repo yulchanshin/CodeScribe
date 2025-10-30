eb4489a46df5cad0c0fdd38eef2782e414254a07
yulcahnshin <yulchanshin@gmail.com>
2025-10-30 00:56:02 -0400

    added code execution/preview

--- src/index.tsx
+++ src/index.tsx
@@ -10,8 +10,8 @@ const root = ReactDOM.createRoot(el!);
 
 const App = () => {
   const ref = useRef<any>();
+  const iframe = useRef<any>();
   const [input, setInput] = useState('');
-  const [code, setCode] = useState('');
 
   //initializing esbuild
   const startService = async () => {
@@ -31,6 +31,8 @@ const App = () => {
       return;
     }
 
+    iframe.current.srcdoc = html;
+
     const result = await ref.current.build({
       entryPoints: ['index.js'],
       bundle: true,
@@ -47,16 +49,42 @@ const App = () => {
 
     // console.log(result);
 
-    setCode(result.outputFiles[0].text);
+    // setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
   };
+
+  //passing the input down to the child iframe
+  const html = `
+    <html>
+      <head></head>
+      <body>
+        <div id="root"></div>
+        <script>
+          window.addEventListener('message', (event) => {
+          try{
+            eval(event.data);
+          } catch (err){
+             const root = document.querySelector('#root');
+             root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>'
+             console.error(err);}
+          }, false)
+        </script>
+      </body>
+    </html>
+  `
 
   return <div>
     <textarea value={input} onChange={e => setInput(e.target.value)}></textarea>
     <div>
       <button onClick={onClick}>Submit</button>
     </div>
-    <pre>{code}</pre>
+    <iframe title="preview" ref={iframe} sandbox="allow-scripts" srcDoc={html} />
   </div>
 };
 
+
+
 root.render(<App />)
+
+
+
