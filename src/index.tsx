5ca46e46821c9bf20ffd986ebb0989df85101dec
yulcahnshin <yulchanshin@gmail.com>
2025-10-31 01:52:22 -0400

    added monaco code editor+ prettier formatting + some css

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
+import 'bulmaswatch/superhero/bulmaswatch.min.css'//lowkey change the theme
 import { useState, useEffect, useRef } from 'react';
 import ReactDOM from "react-dom/client";
 import * as esbuild from 'esbuild-wasm'
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 import { fetchPlugin } from './plugins/fetch-plugin';
+import CodeEditor from './components/code-editor';
 
 const el = document.getElementById("root");
 
@@ -74,6 +76,10 @@ const App = () => {
   `
 
   return <div>
+    <CodeEditor
+      initialValue="const a = 1;"
+      onChange={(value) => setInput(value)}
+    />
     <textarea value={input} onChange={e => setInput(e.target.value)}></textarea>
     <div>
       <button onClick={onClick}>Submit</button>
